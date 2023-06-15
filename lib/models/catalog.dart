16e82dd9123c14a3be740beb11a7b9b399ca8270
arnold004 <arnoldpascal01@gmail.com>
2023-06-15 21:25:34 +0530

    CommitDay11

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,19 @@
+class CatalogModel {
+  static final items = [
+    Item(
+      id: 1,
+      name: "iphone 12 Pro",
+      description: "Apple iphone 12th generation",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://media.mobex.in/media/catalog/product/cache/0e8fa5907f13de785507bb9c529c3575/a/p/apple_iphone_12_pro_pacific_blue_256_gb_1.jpg",
+    )
+  ];
+}
+
 class Item {
-  final String id;
+  final num id;
   final String name;
   final String description;
   final num price;
@@ -14,18 +28,3 @@ class Item {
       required this.color,
       required this.image});
 }
-
-final products = [
-  Item(
-    id: "arnold001",
-    name: "iphone 12 Pro",
-    description: "Apple iphone 12th generation",
-    price: 99999,
-    color: "#33505a",
-    image:
-        "https://media.mobex.in/media/catalog/product/cache/0e8fa5907f13de785507bb9c529c3575/a/p/apple_iphone_12_pro_pacific_blue_256_gb_1.jpg",
-  )
-];
-
-
-// 3:22:53
