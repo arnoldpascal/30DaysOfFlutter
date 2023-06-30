1f1475080383a7d9ab59f67e085fba1da2b4a41f
arnold004 <arnoldpascal01@gmail.com>
2023-06-30 13:15:50 +0530

    CommitDay25

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
   // catalog field
@@ -34,4 +36,13 @@ class CartModel {
   }
 }
 
-// 7:06:16
+class AddMutation extends VxMutation<MyStore> {
+  final Item item;
+
+  AddMutation(this.item);
+  @override
+  perform() {
+    // TODO: implement perform
+    store?.cart._itemIds.add(item.id.toInt());
+  }
+}
